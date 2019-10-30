ca56fb4000fdc0d5fa2cd66ea5cd0434bba21569
Matthew Stroble <mssgwb@umsystem.edu>
2019-10-30 18:18:16 -0500

    Initial Commit

---
+++ S&T Live/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  S&T Live
+//
+//  Created by matthew stroble on 10/30/19.
+//  Copyright © 2019 matthew stroble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
