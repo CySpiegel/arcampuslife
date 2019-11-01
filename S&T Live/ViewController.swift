a76a063cf0fdedb65f246ca08841f738bebcae47
Matthew Stroble <mssgwb@umsystem.edu>
2019-10-31 22:47:59 -0500

    Changed to iOS production app

--- S&T Live/ViewController.swift
+++ S&T Live/ViewController.swift
@@ -5,14 +5,31 @@
 //  Created by matthew stroble on 10/30/19.
 //  Copyright © 2019 matthew stroble. All rights reserved.
 //
-
+import ARCL
+import ARKit
+import MapKit
 import UIKit
 
+@available(iOS 11.0, *)
 class ViewController: UIViewController {
 
+    @IBOutlet var contentView: UIView!
+    
+    let sceneLocationView = SceneLocationView()
+    
+    class func loadFromStoryboard() -> ViewController {
+        return UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "ARCLViewController") as!
+        ViewController
+        
+    }
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
