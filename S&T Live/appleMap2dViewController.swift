3feaeddbb5423664b9f315b008082d632f95f29d
Matthew Stroble <mssgwb@umsystem.edu>
2019-11-02 16:45:37 -0500

    updated map view with basic functions.

---
+++ S&T Live/appleMap2dViewController.swift
@@ -0,0 +1,62 @@
+//
+//  appleMap2dViewController.swift
+//  S&T Live
+//
+//  Created by matthew stroble on 11/2/19.
+//  Copyright © 2019 matthew stroble. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class appleMap2dViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapTypeSegmentedControll: UISegmentedControl!
+    @IBOutlet weak var mapView: MKMapView!
+    private let locationManager = CLLocationManager()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.mapView.delegate = self
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        
+        self.mapView.showsUserLocation = true
+        
+        self.mapTypeSegmentedControll.addTarget(self, action: #selector(mapTypeChanged), for: .valueChanged)
+    }
+    
+    @objc func mapTypeChanged(segmentedControl :UISegmentedControl) {
+        
+        switch(segmentedControl.selectedSegmentIndex) {
+        case 0:
+            self.mapView.mapType = .standard
+        case 1:
+            self.mapView.mapType = .satellite
+        case 2:
+            self.mapView.mapType = .hybrid
+        default:
+            self.mapView.mapType = .standard
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span:
+            MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+}
